e2d3f52b806b92e82aa9cff501006e69ced0cd48
Agnia <agnia2000@inbox.ru>
2020-07-29 00:36:48 +0300

    feat: change layout main

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './component/header/index';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./component/header/index";
+import Question from "./component/question/index";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="songbird-app">
       <Header></Header>
+      <Container>
+        <Row>
+          <Question></Question>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+        <Row></Row>
+      </Container>
     </div>
   );
 }
--- songbird/src/component/question/index.jsx
+++ songbird/src/component/question/index.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import logo from './assets/images/logo.jpg';
+import React, { Component } from "react";
+import logo from "./assets/images/logo.jpg";
 
-import {
-  Navbar, Nav, Badge, Container, Row, Col
-} from 'react-bootstrap';
-import './styles.scss';
+import { Navbar, Nav, Badge, Container, Row, Col } from "react-bootstrap";
+import "./styles.scss";
 
-
-class Header extends Component {
+class Question extends Component {
   render() {
     return (
       <header className="header">
@@ -18,20 +15,37 @@ class Header extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto header-nav">
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Разминка</Nav.Link>
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Воробьиные</Nav.Link>
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Лесные птицы</Nav.Link>
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Певчие птицы</Nav.Link>
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Хищные птицы</Nav.Link>
-              <Nav.Link className="mr-auto header-nav__link" href="/#">Морские птицы</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Разминка
+              </Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Воробьиные
+              </Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Лесные птицы
+              </Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Певчие птицы
+              </Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Хищные птицы
+              </Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">
+                Морские птицы
+              </Nav.Link>
             </Nav>
 
-            <h4>Score: <Badge variant="secondary" className="header-score">0</Badge></h4>
+            <h4>
+              Score:{" "}
+              <Badge variant="secondary" className="header-score">
+                0
+              </Badge>
+            </h4>
           </Navbar.Collapse>
         </Navbar>
-      </header >
+      </header>
     );
   }
 }
 
-export default Header;
+export default Question;
