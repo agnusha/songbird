8056d75b1e29dfdb6fc957324c9f0d31ae6c7faf
Агния Старовойтова <agniastar@outlook.com>
2020-07-28 17:30:49 +0300

    feat: finish header

--- songbird/src/component/header/index.jsx
+++ songbird/src/component/header/index.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import logo from './assets/images/logo.jpg';
+
+import {
+  Navbar, Nav, Badge, Container, Row, Col
+} from 'react-bootstrap';
+import './styles.scss';
 
 
 class Header extends Component {
   render() {
     return (
-      <div className='container'>
+      <header className="header">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">
+            <img src={logo} className="songbird-logo" alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto header-nav">
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Разминка</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Воробьиные</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Лесные птицы</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Певчие птицы</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Хищные птицы</Nav.Link>
+              <Nav.Link className="mr-auto header-nav__link" href="/#">Морские птицы</Nav.Link>
+            </Nav>
 
-      </div>
+            <h4>Score: <Badge variant="secondary" className="header-score">0</Badge></h4>
+          </Navbar.Collapse>
+        </Navbar>
+      </header >
     );
   }
 }
-asdasfda
 
 export default Header;
--- songbird/src/index.js
+++ songbird/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './reset.scss';
+import './styles.scss';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
