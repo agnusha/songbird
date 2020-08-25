dfef299a27e74cb98e7a2cfcf2c54f5a0d8c7954
Agnia <agnia2000@inbox.ru>
2020-08-26 00:56:31 +0300

    fix: play audio

--- songbird/src/component/option/index.jsx
+++ songbird/src/component/option/index.jsx
@@ -16,41 +16,22 @@ class Option extends Component {
     onClick: PropTypes.func,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.audioRefCorrect = React.createRef();
-    this.audioRefIncorrect = React.createRef();
-  }
-
-  playAudio = (isNowRightAnswer) => {
-    if (isNowRightAnswer) {
-      this.audioRefCorrect.current.play();
-    } else {
-      this.audioRefIncorrect.current.play();
-    }
-  };
-
   selectOption = (isItWasAnswer, isNowRightAnswer, selectedBirdId) => {
     if (!this.props.isCurrentBirdGuessed) {
-      this.playAudio(isNowRightAnswer);
+      const audio = new Audio(isNowRightAnswer ? rightAnswer : wrongAnswer);
+      audio.play();
     }
     this.props.onClick(isItWasAnswer, isNowRightAnswer, selectedBirdId);
   };
 
   render() {
-    const {
-      items, currentRightItemNumber, isCurrentBirdGuessed,
-    } = this.props;
+    const { items, currentRightItemNumber, isCurrentBirdGuessed } = this.props;
 
     return (
       <Container className="rounded-container option main-container">
         {items.map((item, i) => <Checkbox key={i} id={item.id} name={item.name}
           currentRightItemNumber={currentRightItemNumber} isCurrentBirdGuessed={isCurrentBirdGuessed}
           onClick={this.selectOption} />)}
-
-        <audio src={rightAnswer} ref={this.audioRefCorrect} />
-        <audio src={wrongAnswer} ref={this.audioRefIncorrect} />
       </Container>
     );
   }
